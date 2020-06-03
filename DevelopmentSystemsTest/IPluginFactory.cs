1ff2ee3b8cf72b56428908320a2c37ffc5ca838f
Tenax <mrbord30@yandex.ru>
2020-06-03 17:08:06 +0300

    Добавьте файлы проекта.

---
+++ DevelopmentSystemsTest/IPluginFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelopmentSystemsTest
+{
+    interface IPluginFactory
+    {
+        int PluginsCount { get; }
+        string[] GetPluginNames { get; }
+        IPlugin GetPlugin(string pluginName);
+
+    }
+}
