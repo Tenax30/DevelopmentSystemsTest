7badecc34c9e5763a6f058204b9fb5887feeab5f
Tenax <mrbord30@yandex.ru>
2020-06-03 19:05:29 +0300

    add static class Plugins

---
+++ DevelopmentSystemsTest/Plugins.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ds.test.impl
+{
+    static class Plugins
+    {
+        public static int PluginsCount { get; }
+        public static string[] GetPluginNames { get; }
+        public static IPlugin GetPlugin(string pluginName)
+        {
+
+        }
+    }
+}
