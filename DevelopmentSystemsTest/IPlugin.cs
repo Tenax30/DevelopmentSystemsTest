07d971b536826c4416de12614f03a43d0e2e6386
Tenax <mrbord30@yandex.ru>
2020-06-03 17:20:10 +0300

    change namespace to ds.test.impl

--- DevelopmentSystemsTest/IPlugin.cs
+++ DevelopmentSystemsTest/IPlugin.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Drawing;
-namespace DevelopmentSystemsTest
+namespace ds.test.impl
 {
     interface IPlugin
     {
--- DevelopmentSystemsTest/IPluginFactory.cs
+++ DevelopmentSystemsTest/IPluginFactory.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DevelopmentSystemsTest
+namespace ds.test.impl
 {
     interface IPluginFactory
     {
